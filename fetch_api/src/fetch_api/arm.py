b81ee9027b02bf067cbb09981b7aeb1c89cc278e
Tracy Tran <tracyttran@github.com>
2018-02-01 12:38:33 -0800

    lab 20: pose planning works

--- fetch_api/src/fetch_api/arm.py
+++ fetch_api/src/fetch_api/arm.py
@@ -67,26 +67,74 @@ class Arm(object):
         # Wait for result
         self._client.wait_for_result()
 
-    def move_to_pose(self, pose_stamped):
+    def move_to_pose(self,
+                 pose_stamped,
+                 allowed_planning_time=10.0,
+                 execution_timeout=15.0,
+                 group_name='arm',
+                 num_planning_attempts=1,
+                 plan_only=False,
+                 replan=False,
+                 replan_attempts=5,
+                 tolerance=0.01):
 
         """Moves the end-effector to a pose, using motion planning.
-        
+
         Args:
             pose: geometry_msgs/PoseStamped. The goal pose for the gripper.
+            allowed_planning_time: float. The maximum duration to wait for a
+                planning result, in seconds.
+            execution_timeout: float. The maximum duration to wait for
+                an arm motion to execute (or for planning to fail completely),
+                in seconds.
+            group_name: string. Either 'arm' or 'arm_with_torso'.
+            num_planning_attempts: int. The number of times to compute the same
+                plan. The shortest path is ultimately used. For random
+                planners, this can help get shorter, less weird paths.
+            plan_only: bool. If True, then this method does not execute the
+                plan on the robot. Useful for determining whether this is
+                likely to succeed.
+            replan: bool. If True, then if an execution fails (while the arm is
+                moving), then come up with a new plan and execute it.
+            replan_attempts: int. How many times to replan if the execution
+                fails.
+            tolerance: float. The goal tolerance, in meters.
+
         Returns:
             string describing the error if an error occurred, else None.
         """
+
         goal_builder = MoveItGoalBuilder()
         goal_builder.set_pose_goal(pose_stamped)
+        goal_builder.allowed_planning_time = allowed_planning_time
+        goal_builder.num_planning_attempts = num_planning_attempts
+        goal_builder.plan_only = plan_only
+        goal_builder.replan = replan
+        goal_builder.replan_attempts = replan_attempts
+        goal_builder.tolerance = tolerance
         goal = goal_builder.build()
+
         self._move_group_client.send_goal(goal)
-        self._move_group_client.wait_for_result(rospy.Duration(10))
+        self._move_group_client.wait_for_result(rospy.Duration(execution_timeout))
+
         result = self._move_group_client.get_result()
         error_code = self.moveit_error_string(result.error_code.val)
         if error_code == 'SUCCESS':
             return None
         else:
             return error_code
+
+    def check_pose(self, 
+               pose_stamped,
+               allowed_planning_time=10.0,
+               group_name='arm',
+               tolerance=0.01):
+        return self.move_to_pose(
+            pose_stamped,
+            allowed_planning_time=allowed_planning_time,
+            group_name=group_name,
+            tolerance=tolerance,
+            plan_only=True)
 
 
     def cancel_all_goals(self):
