08ff62a7038a1701497c9785c66ff9a93fa9b0e5
Tracy Tran <tracyttran@github.com>
2018-02-01 12:48:16 -0800

    lab 21: failing to clear divider even with torso height at max

--- applications/scripts/arm_obstacle_demo.py
+++ applications/scripts/arm_obstacle_demo.py
@@ -25,8 +25,7 @@ def main():
     table_x = 0.8
     table_y = 0
     table_z = 0.6
-    planning_scene.addBox('table', table_size_x, table_size_y, table_size_z,
-                        table_x, table_y, table_z)
+    planning_scene.addBox('table', table_size_x, table_size_y, table_size_z, table_x, table_y, table_z)
 
     # Create divider obstacle
     planning_scene.removeCollisionObject('divider')
@@ -37,6 +36,54 @@ def main():
     y = 0 
     z = table_z + (table_size_z / 2) + (size_z / 2)
     planning_scene.addBox('divider', size_x, size_y, size_z, x, y, z)
+
+    pose1 = PoseStamped()
+    pose1.header.frame_id = 'base_link'
+    pose1.pose.position.x = 0.5
+    pose1.pose.position.y = -0.3
+    pose1.pose.position.z = 0.75
+    pose1.pose.orientation.w = 1
+
+    pose2 = PoseStamped()
+    pose2.header.frame_id = 'base_link'
+    pose2.pose.position.x = 0.5
+    pose2.pose.position.y = 0.3
+    pose2.pose.position.z = 0.75
+    pose2.pose.orientation.w = 1
+
+    arm = fetch_api.Arm()
+
+    def shutdown():
+        arm.cancel_all_goals()
+
+    rospy.on_shutdown(shutdown)
+
+    kwargs = {
+        'allowed_planning_time': 15,
+        'execution_timeout': 10,
+        'num_planning_attempts': 5,
+        'replan': False
+    }
+
+    error = arm.move_to_pose(pose1, **kwargs)
+
+    if error is not None:
+        rospy.logerr('Pose 1 failed: {}'.format(error))
+    else:
+        rospy.loginfo('Pose 1 succeeded')
+
+    rospy.sleep(1)
+
+    error = arm.move_to_pose(pose2, **kwargs)
+
+    if error is not None:
+        rospy.logerr('Pose 2 failed: {}'.format(error))
+    else:
+        rospy.loginfo('Pose 2 succeeded')
+
+
+    planning_scene.removeCollisionObject('table')
+    planning_scene.removeCollisionObject('divider')
 
     
 
