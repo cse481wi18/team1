b37c5278a37963b4325489984bbb73bf27f1310c
Tracy Tran <tracyttran@github.com>
2018-02-01 12:24:30 -0800

    Revert "lab 19 done"
    This reverts commit 97b534cd74911c791ee2010f3fe46a2745d029d6.

--- fetch_api/src/fetch_api/__init__.py
+++ fetch_api/src/fetch_api/__init__.py
@@ -3,5 +3,4 @@ from .gripper import Gripper
 from .torso import Torso
 from .head import Head
 from .arm import Arm
-from .moveit_goal_builder import MoveItGoalBuilder
 from .arm_joints import ArmJoints
