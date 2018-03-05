87a5487229cd4416e90d1f795b91a71128c9c10b
Tracy Tran <tracyttran@github.com>
2018-03-04 23:30:02 -0800

    navigator, added navigator to cleaning manager

--- cleaning_manager/src/cleaning_manager_setup.py
+++ cleaning_manager/src/cleaning_manager_setup.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 import rospy
+from perception import 
 from std_msgs.msg import Int64
+import navigator import Navigator
 import time
 
 
@@ -11,28 +13,50 @@ def wait_for_time():
         pass
 
 class CleaningManager(object):
-    def __init__(self, segmenter):
+    def __init__(self, segmenter, navigator):
         self._requests = []
         self._cleaning = False
         self._segmenter = segmenter
+        self._navigator = navigator
     
-    def callback(self, data):
+
+    def qr_callback(self, data):
         rospy.loginfo('Received data: %d', data.data)
         self._requests.append(data.data)
         if not self._cleaning:
             self._cleaning = True
             print "starting to clean"
+            self._start_cleaning_sequence()
+
         else:
             print "Already cleaning"
 
+
     def start(self):
-        rospy.Subscriber('qr_code_interface', Int64, callback=self.callback)
+        rospy.Subscriber('qr_code_interface', Int64, callback=self.qr_callback)
+    
+
+    def start_cleaning_sequence(self):
+        result = self._navigator.goto(self._requests.pop(0))
+            if result == 1:
+                # START SWEEP SEQUENCE
+            else: 
+                # RETRY? 
+
+        
+
+
+        if len(self._requests) > 0:
+            start_cleaning_sequence()
+
+
 
 def main():
     rospy.init_node('cleaning_manager')
     wait_for_time()
 
     segmenter = segmenter(table_pub, marker_pub, object_pub, coord_pub)
+    navigator = Navigator(15) # 15 second timeout for each pose
 
     manager = CleaningManager(segmenter)
     manager.start()
--- navigator/src/navigator/Navigator.py
+++ navigator/src/navigator/Navigator.py
@@ -6,24 +6,27 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, PoseWithCovariance, Pos
 
 class Navigator(object):     
 
-     def _callback(self, data):
-        # data is of type PoseWithCovarianceStamped, which has a PoseWithCovariance, which has a Pose
-        self._last_pose = data.pose.pose # type Pose, which has Point and Quaternion
+    def _current_pose_callback(self, data):
+    # data is of type PoseWithCovarianceStamped, which has a PoseWithCovariance, which has a Pose
+    self._last_pose = data.pose.pose # type Pose, which has Point and Quaternion
     
-    def __init__(self):
+
+    def __init__(self, timeout):
 
         # map from table number to table pose
         self._saved_poses = self._pickle_load("tables.p")
 
         self._last_pose = None
         # acml_pose topic gives current pose
-        rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self._callback)
+        rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self._current_pose_callback)
 
         # publish poses to move_base_simple/goal which will move the base (node nav_rviz does this)
         self._pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size = 10)
 
+        self._timeout = timeout
+
     # name = table number
-    # returns -1 if name is not a saved pose
+    # returns -1 if name is not a saved pose, 1 if successfully moved to pose, 0 if couldn't move to pose
     def goto(self, name):
         if name not in self._saved_poses:
             return -1
@@ -32,10 +35,21 @@ class Navigator(object):
         msg.pose = self._saved_poses[name]
         msg.header.frame_id = "map"
         self._pub.publish(msg)
-        return 0
+
+        start_time = rospy.get_rostime()
+
+        while self._distance(self._last_pose, msg.pose) > 0.1 and (rospy.get_rostime() - start_time < self._timeout):
+            rospy.sleep(0.5) # sleep for 0.5 seconds
+        
+        if self._distance(self._last_pose, msg.pose) < 0.1:
+            return 1
+        else:
+            return 0
     
+
     def pickle_dump(self, file):
         pickle.dump(self._saved_poses, open(file, 'wb'))
+
 
     def pickle_load(self, file):
         try:
