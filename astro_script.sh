85c9857cb7035db82892792ab88642351df8b555
Tracy Tran <tracyttran@github.com>
2018-03-12 21:06:28 -0700

    launch script

--- astro_script.sh
+++ astro_script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-xterm -title "Rviz" -hold -e 'sleep 5; setrobot astro; rosrun rviz rviz -d /home/cse481cwi18/Desktop/team1.rviz' &
+xterm -title "Rviz" -hold -e "sleep 5; setrobot astro 'rosrun rviz rviz -d /home/cse481cwi18/Desktop/team1.rviz'" &
 
-xterm -title "MoveIt" -hold -e 'ssh team1@astro; roslaunch fetch_api move_group.launch' &
+xterm -title "MoveIt" -hold -e "ssh team1@astro 'roslaunch fetch_api move_group.launch'" &
 
-xterm -title "Nav Map" -hold -e 'ssh team1@astro; roslaunch applications sieg_nav.launch' &
+xterm -title "Nav Map" -hold -e "ssh team1@astro 'roslaunch applications sieg_nav.launch'" &
 
-xterm -title "Perception" -hold -e 'roslaunch perception table_recognizer.launch is_sim:=false' &
+xterm -title "Perception" -hold -e "ssh team1@astro 'roslaunch perception table_recognizer.launch is_sim:=false'" &
 
-xterm -title "Action Demo" -hold -e 'sleep 10; rosrun applications action_demo.py' &
+xterm -title "Action Demo" -hold -e "sleep 10; ssh team1@astro 'rosrun applications action_demo.py'" &
 
-xterm -title "Nav Demo" -hold -e 'sleep 10; ssh team1@astro; rosrun applications navigation_demo.py' &
+xterm -title "Nav Demo" -hold -e "sleep 10; ssh team1@astro 'rosrun applications navigation_demo_astro.py'" &
+
+xterm -title "Cleaning Demo" -hold -e "sleep 10; ssh team1@astro 'rosrun cleaning_manager cleaning_manager_setup.py'" &
